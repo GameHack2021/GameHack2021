94723ebf1ae7080dac84be7e13a415708454a2a4
BonnyWang <bonny.y.gardy@gmail.com>
2021-06-18 13:19:13 -0400

    added cat

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : Character
 {
 
+    int cat_Carried;
     void Start()
     {
 
@@ -17,6 +18,8 @@ public class Player : Character
         canWalk = true;
         canJump = true;
         onGround = true;
+
+        cat_Carried = 0;
     }
 
     // Update is called once per frame
