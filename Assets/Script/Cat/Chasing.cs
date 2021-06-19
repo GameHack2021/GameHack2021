76a6c665b56eaf4c25e0f404f0121b4dd35466d9
dannyHallo <2759370364@qq.com>
2021-06-19 13:14:18 +0800

    Update Chasing.cs

--- Assets/Script/Cat/Chasing.cs
+++ Assets/Script/Cat/Chasing.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Chasing : MonoBehaviour
 {
+    public float minChasingDistanceInX = 4.0f;
+    public float minChasingDistanceInY = 3.5f;
+    public float chasingSpeed = 2.0f;
+
     GameObject player;
     Rigidbody2D rb;
     Collider2D playerCollider;
     Collider2D myCollider;
+    
+    float previousY = 0;
+
+    bool jumping = false;
     private void Awake()
     {
         player = GameObject.Find("Player");
@@ -24,20 +32,56 @@ public class Chasing : MonoBehaviour
     void CounterMovement()
     {
         float dst = Vector2.Distance(player.transform.position, transform.position);
-        if (dst >= 4f)
-            Chase(dst);
+        float dstX = Mathf.Abs(player.transform.position.x - transform.position.x);
+        float dstY = Mathf.Abs(player.transform.position.y - transform.position.y);
+        if (dstX >= minChasingDistanceInX)
+            ChaseInX(dstX);
+        if (dstY >= minChasingDistanceInY)
+            ChaseInY(dstY);
     }
 
-    void Chase(float dst)
+    void ChaseInX(float dst)
     {
         Vector2 direction = (player.transform.position - transform.position);
-        Vector2 xDirection = new Vector2();
-        Vector2 yDirection = new Vector2();
-        xDirection.x = direction.x;
-        xDirection = xDirection.normalized;
-        yDirection.y = direction.y;
-        yDirection = yDirection.normalized;
 
-        rb.velocity = xDirection * 2f * (dst - 4);
+        float xDirection;
+
+        if (direction.x > 0)
+        {
+            xDirection = 1;
+        }
+        else
+        {
+            xDirection = -1;
+        }
+
+        Vector2 aimVelocity = new Vector2();
+        aimVelocity.x = xDirection * chasingSpeed * (dst - minChasingDistanceInX);
+        aimVelocity.y = rb.velocity.y;
+
+        rb.velocity = aimVelocity;
+    }
+
+    void ChaseInY(float dst)
+    {
+        previousY = dst;
+        
+        Vector2 direction = (player.transform.position - transform.position);
+
+        if (dst - previousY < 0 && !jumping)    
+        {
+            Vector2 aimVelocity = new Vector2();
+            aimVelocity.x = rb.velocity.x;
+            aimVelocity.y = 15f;
+            jumping = true;
+            rb.velocity = aimVelocity;
+        }
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumping = false;
     }
 }
