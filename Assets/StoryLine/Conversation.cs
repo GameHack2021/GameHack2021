c70a6752672bdec0539ea81364a089b968c64301
BonnyWang <bonny.y.gardy@gmail.com>
2021-06-19 11:33:16 -0400

    storyline created

---
+++ Assets/StoryLine/Conversation.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conversation : MonoBehaviour
+{
+    string[] characters = new string[]{"Player","Cat"};
+    string[] conversations = new string[]{"Hi", "How are you"};
+}
