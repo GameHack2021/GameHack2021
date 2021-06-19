21fa1140da92e6167aa351386e2ab503b2d4c37a
dannyHallo <2759370364@qq.com>
2021-06-19 21:28:50 +0800

    Update Chasing.cs

--- Assets/Script/Cat/Chasing.cs
+++ Assets/Script/Cat/Chasing.cs
@@ -4,37 +4,103 @@ using UnityEngine;
 
 public class Chasing : MonoBehaviour
 {
+    public float minChasingDistanceInX = 4.0f;
+    public float minChasingDistanceInY = 3.5f;
+    public float chasingSpeed = 2.0f;
+    public float jumpingVel = 12f;
+
     GameObject player;
     Rigidbody2D rb;
+    Collider2D playerCollider;
+    Collider2D myCollider;
 
+    float previousY = 0;
+    Vector2 direction = new Vector2();
+    float dstX = 0;
+    float dstY = 0;
+
+    bool jumping = false;
+    bool focused = false;
     private void Awake()
     {
         player = GameObject.Find("Player");
+        playerCollider = player.GetComponent<Collider2D>();
+        myCollider = GetComponent<Collider2D>();
         rb = gameObject.GetComponent<Rigidbody2D>();
-
+        Physics2D.IgnoreCollision(playerCollider, myCollider, true);
     }
     private void Update()
     {
-        CounterMovement();
+        direction = (player.transform.position - transform.position);
+        dstX = Mathf.Abs(player.transform.position.x - transform.position.x);
+        dstY = Mathf.Abs(player.transform.position.y - transform.position.y);
+
+        if (focused)
+            CounterMovement();
+        else
+            CheckFocus();
     }
 
     void CounterMovement()
     {
-        float dst = Vector2.Distance(player.transform.position, transform.position);
-        if (dst >= 5f)
-            Chase();
+        if (dstX >= minChasingDistanceInX)
+            ChaseInX(dstX);
+        if (dstY >= minChasingDistanceInY)
+            ChaseInY(dstY);
     }
 
-    void Chase()
+    void ChaseInX(float dst)
     {
-        Vector2 direction = (player.transform.position - transform.position);
-        Vector2 xDirection = new Vector2();
-        Vector2 yDirection = new Vector2();
-        xDirection.x = direction.x;
-        xDirection = xDirection.normalized;
-        yDirection.y = direction.y;
-        yDirection = yDirection.normalized;
+        float xDirection;
 
-        rb.velocity = xDirection * 2f;
+        if (direction.x > 0)
+        {
+            xDirection = 1;
+        }
+        else
+        {
+            xDirection = -1;
+        }
+
+        Vector2 aimVelocity = new Vector2();
+        aimVelocity.x = xDirection * chasingSpeed * (dst - minChasingDistanceInX);
+        aimVelocity.y = rb.velocity.y;
+
+        rb.velocity = aimVelocity;
+    }
+
+    void ChaseInY(float dst)
+    {
+        if (dst - previousY < 0 && !jumping)
+        {
+            Vector2 aimVelocity = new Vector2();
+            aimVelocity.x = rb.velocity.x;
+            aimVelocity.y = jumpingVel;
+            jumping = true;
+            rb.velocity = aimVelocity;
+        }
+        print(dst - previousY);
+        previousY = dst;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumping = false;
+        CheckFocus();
+
+    }
+
+    void CheckFocus()
+    {
+        previousY = 0;
+        if (dstX <= 10f && dstY <= 2f)
+        {
+            focused = true;
+        }
+        else
+        {
+            focused = false;
+        }
     }
 }
