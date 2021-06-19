4fbf376cbcc0739171d14b7b82241644600a94f8
dannyHallo <2759370364@qq.com>
2021-06-19 21:54:02 +0800

    af
    af

---
+++ Assets/Script/GameManagers/StartGameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGameManager : MonoBehaviour
+{
+    Button start;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        start = GameObject.Find("Canvas/StartButton").GetComponent<Button>();
+        start.onClick.AddListener(ChangeScene);
+    }
+
+    void ChangeScene()
+    {
+        print(1);
+        SceneManager.LoadScene("Assets/Scenes/SampleScene.unity", LoadSceneMode.Additive);
+    }
+}
