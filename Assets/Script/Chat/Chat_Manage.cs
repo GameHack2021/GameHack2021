dcefa9cef7ed0475e83040187347daeeed46de17
dannyHallo <2759370364@qq.com>
2021-06-19 14:15:54 +0800

    hello

--- Assets/Script/Chat/Chat_Manage.cs
+++ Assets/Script/Chat/Chat_Manage.cs
@@ -4,5 +4,43 @@ using UnityEngine;
 
 public class Chat_Manage : MonoBehaviour
 {
-//    If(Input.GetButton)
+   GameObject Player;
+   GameObject Cat; 
+
+   private void Start() {
+      Player = GameObject.Find("Player");
+      Cat = GameObject.Find("Cat");
+      intialization();
+   }
+   private void Update() {
+
+      if(Input.GetButtonDown("Submit")){
+         nextConversation();
+      }
+   }
+
+   void intialization(){
+      Player.SetActive(true);
+      Cat.SetActive(false);
+   }
+
+   void nextConversation(){
+      if(Player.active){
+         Cat.SetActive(true);
+         Player.SetActive(false);
+         generateCatTalk();
+      }else{
+         Cat.SetActive(false);
+         Player.SetActive(true);
+         generateDogTalk();
+      }
+   }
+
+   void generateCatTalk(){
+      // TODO:call api
+   }
+
+   void generateDogTalk(){
+
+   }
 }
