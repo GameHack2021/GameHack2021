a61ffd4b0cf5d77eeecbd5b66415e26e72f4b4d4
BonnyWang <bonny.y.gardy@gmail.com>
2021-06-20 03:12:20 -0400

    fff

--- Assets/Script/Player/Player_Interaction.cs
+++ Assets/Script/Player/Player_Interaction.cs
@@ -2,35 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class Player_Interaction : MonoBehaviour
 {
     Player player;
     Text catNumber;
-    Text catsSentSuccessfully;
-    Text timeShowing;
 
-    float timeLimitation = 10f;
-    float timeStamp;
     // Interaction objects variablex
     public int catsToTake;
     public int cat_Carried;
     public int cat_NeedToGive = 1;
     public int cat_Sent = 0;
+
     bool canTakeCats;
 
     private void Awake()
     {
         catNumber = GameObject.Find("Canvas/MainSceneUI/catNumber").GetComponent<Text>();
-        catsSentSuccessfully = GameObject.Find("Canvas/MainSceneUI/catsSentSuccessfully").GetComponent<Text>();
-        timeShowing = GameObject.Find("Canvas/MainSceneUI/time").GetComponent<Text>();
-        timeStamp = timeLimitation + 1;
     }
 
     void Start()
     {
         player = GetComponent<Player>();
+
         // initialize variables
         canTakeCats = true;
         
@@ -39,11 +33,11 @@ public class Player_Interaction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeStamp -= Time.deltaTime;
         catNumber.text = "Cats left: " + cat_Carried;
+
         catsSentSuccessfully.text = "Cats sent: " + cat_Sent;
         timeShowing.text = ((int)timeStamp).ToString();
-<<<<<<< HEAD
+
         if(cat_Sent >= cat_NeedToGive)
         {
             LoadWinScene();
@@ -52,8 +46,7 @@ public class Player_Interaction : MonoBehaviour
         {
             LoadLoseScene();
         }
-=======
->>>>>>> parent of b2ce6a9 (sine)
+
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
@@ -67,7 +60,8 @@ public class Player_Interaction : MonoBehaviour
             cat_Carried = cat_Carried + 1;
         }
     }
-<<<<<<< HEAD
+
+
 
     void LoadWinScene()
     {
@@ -79,7 +73,3 @@ public class Player_Interaction : MonoBehaviour
         SceneManager.LoadScene("endS");
     }
 
-
-=======
->>>>>>> parent of b2ce6a9 (sine)
-}
